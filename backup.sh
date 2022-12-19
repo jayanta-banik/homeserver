9e0414258375ec9e97ecfc3f190431039eef1d5c
Jayanta Banik <35198097+jayanta-banik@users.noreply.github.com>
2022-12-19 01:07:10 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
+echo "Enter git token"
+read gitoken
+uname="jayanta-banik"
 now=`date`
-token=`cat auths/git_push`
-git add .
-git reset -- homeserver/venv3/*
-git reset -- homeserver/auths/*
+
+git add -p . : ^homeserver/venv3
+
 git commit -m "$now"
-echo "Enter token"
-echo "$token"
-git push
+git push https://$uname:$gitoken@github.com/jayanta-banik/homeserver.git
