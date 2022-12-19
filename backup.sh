3c233f74f93683dcf5933bf63f107f1e1b6510b8
jayanta-banik <sciencenerd1609@gmail.com>
2022-12-18 20:38:03 -0800

    Sun 18 Dec 2022 08:37:40 PM PST

--- backup.sh
+++ backup.sh
@@ -1,15 +1,7 @@
-
+now=`date`
 git add .
 git reset -- homeserver/venv3/*
-git commit -m Sun 18 Dec 2022 06:53:33 PM PST LA, USA
-
-git add .
-git reset -- homeserver/venv3/*
-git commit -m "Sun 18 Dec 2022 06:54:15 PM PST LA, USA"
-
-git add .
-git reset -- homeserver/venv3/*
-git commit -m "Sun 18 Dec 2022 06:56:36 PM PST"
+git commit -m "$now"
 echo "Enter Cred 
 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"
 git push
