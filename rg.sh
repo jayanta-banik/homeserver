2a90054d6425d219a12952f66038a0770e1ef93a
jayanta-banik <sciencenerd1609@gmail.com>
2022-12-19 16:29:05 -0800

    Mon 19 Dec 2022 04:29:05 PM PST

---
+++ rg.sh
@@ -0,0 +1,29 @@
+echo "restarting https"
+sudo systemctl daemon-reload
+sudo systemctl start app
+sudo systemctl enable app
+sudo systemctl restart app
+echo "done"
+
+echo "restarting http"
+sudo systemctl daemon-reload
+sudo systemctl start app_http
+sudo systemctl enable app_http
+sudo systemctl restart app_http
+echo "done"
+
+echo "restarting http"
+sudo systemctl daemon-reload
+sudo systemctl start app_root
+sudo systemctl enable app_root
+sudo systemctl restart app_root
+echo "done"
+
+echo "restarting http"
+sudo systemctl daemon-reload
+sudo systemctl start ai
+sudo systemctl enable ai
+sudo systemctl restart ai
+sudo systemctl restart nginx
+echo "done"
+
