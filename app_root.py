228994723e8bb26ea2040ec5b98c8e0da12b8f8d
jayanta-banik <sciencenerd1609@gmail.com>
2022-12-23 00:53:34 -0800

    Fri 23 Dec 2022 12:53:30 AM PST

--- app_root.py
+++ app_root.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from libs import AuthCheck
+
+
 app_root = Flask(__name__)
 @app_root.route('/')
+
+
 def hello_world():
-    return "Root server"
+    return "Root server: %s"%AuthCheck.sudo_privileges
 if __name__ == '__main__':
     app_root.run(debug=True, host='0.0.0.0')
