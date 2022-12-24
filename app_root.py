55071864897a8e93b27d747f19a5fb8942228fda
jayanta-banik <sciencenerd1609@gmail.com>
2022-12-23 22:29:21 -0800

    Fri 23 Dec 2022 10:29:18 PM PST

--- app_root.py
+++ app_root.py
@@ -1,10 +1,24 @@
+# library
+import board
+import neopixel
+from time import sleep
 from flask import Flask
+
+# my library
 from libs import AuthCheck
 
-
+# variables
+sudo_privileges = AuthCheck.sudo_privileges
 app_root = Flask(__name__)
+
+# routes
 @app_root.route('/')
-def hello_world():
-    return "Root server: %s"%AuthCheck.sudo_privileges
+def helloWorld():
+    return "unauthorised access"
+@app_root.route('/access_check')
+def print_access():
+    return "Root server: %s"%sudo_privileges
+
+# main
 if __name__ == '__main__':
     app_root.run(debug=True, host='0.0.0.0')
